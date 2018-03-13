0301f68428b89aa878a1932222767f16c9971e9d
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2018-03-13 22:28:16 +0300

    create base rectangle from dlib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,19 +4,23 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 
+#include <dlib/geometry/rectangle.h>
+
 using namespace std;
 using namespace cv;
+using namespace dlib;
 
 extern "C"
 {
-void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
-                                                                           jlong matAddrGray,
-                                                                           jint nbrElem) {
-    Mat &mGr = *(Mat *) matAddrGray;
-    for (int k = 0; k < nbrElem; k++) {
-        int i = rand() % mGr.cols;
-        int j = rand() % mGr.rows;
-        mGr.at<uchar>(j, i) = 255;
+    void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                                               jlong matAddrGray,
+                                                                               jint nbrElem) {
+        dlib::rectangle rec(0,0,100,100);
+        Mat &mGr = *(Mat *) matAddrGray;
+        for (int k = 0; k < nbrElem; k++) {
+            int i = rand() % mGr.cols;
+            int j = rand() % mGr.rows;
+            mGr.at<uchar>(j, i) = 255;
+        }
     }
-}
 }
